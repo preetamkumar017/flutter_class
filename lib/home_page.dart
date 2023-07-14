7f04efac6c53bd055fd014dd6b5f5c3558b188db
preetamkumar017 <preetamkumar017@gmail.com>
2023-07-14 22:06:05 +0530

    day 14/07/2023 completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,6 +9,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController name = TextEditingController();
+  bool pass = true;
+
+  @override
+  void initState() {
+    super.initState();
+    name.text = "Preetam sinha";
+  }
+
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
@@ -25,15 +34,86 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.max,
           children: [
+
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child:  TextFormField(
+                decoration:  InputDecoration(
+                  suffixIcon: InkWell(
+                      onTap: () {
+                        pass = !pass;
+                        print(pass);
+                        setState(() {
+
+                        });
+                      },
+                      child: pass ? Icon(Icons.visibility_off) : Icon(Icons.visibility)) ,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                    labelText: 'Enter Name',
+                    hintText: 'Enter Your Name',
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                style: TextStyle(
+                  fontSize: 25
+                ),
+                autofocus: true,
+                keyboardType: TextInputType.name,
+                controller: name,
+                // enabled: false,
+                readOnly: true,
+                obscureText: pass,
+                obscuringCharacter: '#',
+                
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+
             const Center(
                 child: Text(
               "first page",
-              style: TextStyle(fontSize: 40),
+              style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.purple,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.italic,
+                  // letterSpacing: 8,
+                  // wordSpacing: -20,
+                  backgroundColor: Colors.yellow,
+                  shadows: [
+                    Shadow(
+                        color: Colors.blueAccent,
+                        offset: Offset(2, 1),
+                        blurRadius: 10)
+                  ]),
             )),
+            RichText(
+              text: const TextSpan(
+                  text: 'Don\'t have an account?',
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: ' Sign up',
+                      style: TextStyle(color: Colors.blueAccent, fontSize: 20),
+                    ),
+                    TextSpan(
+                      text: ' Sign up',
+                      style: TextStyle(color: Colors.greenAccent, fontSize: 20),
+                    ),
+                  ]),
+            ),
             Center(
               child: ElevatedButton(
                   onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => SecondPage(),));
+                    print(name.text);
+                    // Navigator.push(
+                    //     context,
+                    //     MaterialPageRoute(
+                    //       builder: (context) => const SecondPage(),
+                    //     ));
                   },
                   child: const Text("Go to Next Page")),
             )
